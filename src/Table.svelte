be51dfba75d1f631bf95646001b252568648322e
Hein Dauven <heindauven@gmail.com>
2021-03-09 17:53:43 +0100

    Add component to display project materials and cost.

---
+++ src/Table.svelte
@@ -0,0 +1,65 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import materialStore from "./material-store.js";
+
+  const dispatch = createEventDispatcher();
+  let materials = [];
+  $: totalCost = materials.reduce((acc, { price }) => (acc += price), 0);
+
+  materialStore.subscribe(items => {
+    materials = items;
+  });
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD"
+  });
+
+  function edit(id, name, price) {
+    dispatch("edit", { id, name, price });
+  }
+
+  function remove(id) {
+    materialStore.remove(id);
+    dispatch("remove", { id });
+  }
+</script>
+
+<style>
+  table {
+    width: 100%;
+  }
+  tr {
+    cursor: pointer;
+  }
+  tr:last-of-type {
+    cursor: inherit;
+  }
+</style>
+
+<table class="primary">
+  <thead>
+    <tr>
+      <th>Material</th>
+      <th>Price</th>
+      <th />
+    </tr>
+  </thead>
+  <tbody>
+    {#each materials as material (material.id)}
+      <tr on:click={edit(material.id, material.name, material.price)}>
+        <td>{material.name}</td>
+        <td>{formatter.format(material.price)}</td>
+        <td>
+          <i
+            on:click|stopPropagation={remove(material.id)}
+            class="far fa-trash-alt" />
+        </td>
+      </tr>
+    {/each}
+    <tr>
+      <td>Total</td>
+      <td colspan="2">{formatter.format(totalCost)}</td>
+    </tr>
+  </tbody>
+</table>
